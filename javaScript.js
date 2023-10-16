9807e41139d7bd8cebc5c4bbcf4ac57da86325b5
Brie <briecrafty@gmail.com>
2023-10-15 23:15:24 -0500

    creating the grid cells

--- javaScript.js
+++ javaScript.js
@@ -1,8 +1,14 @@
-const grid=document.querySelector('.grid');
-for(let r=0;r<16;r++){
-    for(let c=0;c<16;c++){
-        const cell=document.createElement('div');
+let cell;
+const grid = document.querySelector('.grid');
+
+
+for (let r = 0; r < 16; r++) {
+    for (let c = 0; c < 16; c++) {
+        cell = document.createElement('div');
         cell.classList.add('cell');
         grid.appendChild(cell);
     }
 }
+
+const listCell = document.querySelectorAll('div.cell');
+
