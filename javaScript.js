547dc7b074ba8b6cbbb0a8c2a5d8936559153bda
Brie <briecrafty@gmail.com>
2023-10-16 00:50:14 -0500

    update eventlistener

--- javaScript.js
+++ javaScript.js
@@ -1,14 +1,19 @@
-let cell;
-const grid = document.querySelector('.grid');
 
+const grid = document.querySelector('.grid');
+// const listCell = document.querySelectorAll('div.cell');
 
 for (let r = 0; r < 16; r++) {
     for (let c = 0; c < 16; c++) {
-        cell = document.createElement('div');
+        let cell = document.createElement('div');
         cell.classList.add('cell');
         grid.appendChild(cell);
+        cell.addEventListener('mousedown', ()=> {
+            cell.style.backgroundColor = "white";
+         },false)
+        
+
     }
 }
 
-const listCell = document.querySelectorAll('div.cell');
+
 
